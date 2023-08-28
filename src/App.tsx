c25f34cf3eeed146e4df24adbd8fae58854d8798
TheBharatMishra <mishrabm2004@gmail.com>
2023-08-28 06:06:23 +0530

    fixed some issues

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <main className="bg-orange-300 h-screen border-8 border-orange-500">
       <section className="bg-orange-500 px-20 py-5 bottom-1/2 left-1/2 absolute">
-        <div>djdjfj</div>
+        <div>{count}</div>
       </section>
       <section>
         <div>+</div>
@@ -17,6 +17,10 @@ function App() {
         <div>DEL</div>
         <div>.</div>
         <div>0</div>
+        <div>1</div>
+        <div>2</div>
+        <div onClick={() => setCount(3)}>3</div>
+        <div>4</div>
       </section>
     </main>
   );
